1653f3d58653a3e628f58919b06a5d0eb03baec0
nephre <m3rl1n0s>
2013-09-20 20:42:57 +0200

    Delete message more verbose: title included

--- src/Merlin/TvShowsBundle/Resources/public/js/list_shows.js
+++ src/Merlin/TvShowsBundle/Resources/public/js/list_shows.js
@@ -2,6 +2,7 @@ $(function() {
     $('a.deleteShow').click(function(e) {
         e.preventDefault();
         var url = $(this).attr('href');
+        var title = $(this).closest('tr').find('td:eq(1)').text();
         var userConfig = {
             captionOk: 'Yes',
             captionCancel: 'No',
@@ -10,7 +11,7 @@ $(function() {
                 location.href = url;
             }
         };
-        var cd = new ConfirmationDialog('Really delete this show?', userConfig);
+        var cd = new ConfirmationDialog('Really delete "' + title +'"?', userConfig);
         cd.show();
     });
 
