bcd4327553415ee63ce4dac42dab1959191e3e0b
nephre <m3rl1n0s>
2013-09-26 11:56:33 +0200

    Confirmation dialog taken from TB

--- src/Merlin/TvShowsBundle/Resources/public/js/list_shows.js
+++ src/Merlin/TvShowsBundle/Resources/public/js/list_shows.js
@@ -1,18 +1,14 @@
 $(function() {
+    var globalUrl;
+
     $('a.deleteShow').click(function(e) {
         e.preventDefault();
         var url = $(this).attr('href');
         var title = $(this).closest('tr').find('td:eq(1)').text();
-        var userConfig = {
-            captionOk: 'Yes',
-            captionCancel: 'No',
-            title: 'Confirmation',
-            onOk: function() {
-                location.href = url;
-            }
-        };
-        var cd = new ConfirmationDialog('Really delete "' + title +'"?', userConfig);
-        cd.show();
+        globalUrl = url;
+
+        $('span#confirmShowName').html(title);
+        $('#confirmationModal').modal('show');
     });
 
     $('a.searchShow').click(function(e) {
@@ -22,4 +18,12 @@ $(function() {
 
         location.href = url;
     });
+
+    $('div#confirmationModal').modal({
+        show: false
+    });
+
+    $('#buttonYes').click(function() {
+        location.href = globalUrl;
+    })
 });
